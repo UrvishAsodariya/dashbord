509ca3f8d03e0691c13d68448a9fdb2087a9e50b
UrvishAsodariya <mrurvishpatel366@gmail.com>
2023-01-19 10:29:37 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './componet/Header';
+import Sidebar from './componet/Sidebar';
+import Deshboard from './componet/Deshboard';
+import Login from './componet/Login';
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-   <>
-    <Header/>
-   </>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Deshboard />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+      {/* <Sidebar /> */}
+    </>
   );
 }
 
